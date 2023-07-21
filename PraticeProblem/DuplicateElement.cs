baae60c51840c5e14e561d921da7e71df8d4091b
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-21 13:22:55 +0530

    [Narenthra kishore] Add : new Solution duplicate array count

---
+++ PraticeProblem/DuplicateElement.cs
@@ -0,0 +1,36 @@
+﻿namespace PraticeProblem
+{
+    internal class DuplicateElement
+    {
+        public static void GetCount(int choice)
+        {
+            Console.WriteLine("Enter length of the array");
+            int length = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[length];
+            Console.WriteLine("Enter the array element");
+            for(int i  = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            Dictionary<string,int> dictionary = new Dictionary<string,int>();
+            foreach (string key in array)
+            {
+                if (dictionary.ContainsKey(key))
+                {
+                    dictionary[key] += 1;
+                }
+                else
+                {
+                    dictionary.Add(key, 1);
+                }
+            }            
+            int count = 0;
+            foreach (var item in dictionary.Keys)
+            {
+                if (dictionary[item] > 1)
+                    count++;
+            }
+            Console.WriteLine("The count of duplicate items are"+count);
+        }
+    }
+}
--- PraticeProblem/Program.cs
+++ PraticeProblem/Program.cs
@@ -6,7 +6,18 @@ namespace PraticeProblem
     {
         public static void Main(string[] args)
         {
-            
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.count of duplicate element");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        DuplicateElement.GetCount(choice);
+                        break;
+                }
+            }
         }
     }
 }
