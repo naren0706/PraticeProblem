e4452a4d95b935555b1279e6c097569d74fbb844
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-24 15:23:20 +0530

    [Narenthra Kishore] modified : Mistakes changed

--- PraticeProblem/lowestSum.cs
+++ PraticeProblem/lowestSum.cs
@@ -6,17 +6,15 @@
         public static void GetLower()
         {
             Console.WriteLine("Enter length of array");
-            //int length = Convert.ToInt32(Console.ReadLine());
-            //int[] arr = new int[length];
-            //int length = 9;
-            int[] arr = { 10,11,23,1, 2, 3, 4, -5, 6, 7, 8, 9};
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[length];
+
             Console.WriteLine("Enter the numbers in array");
             int min = arr[0];
             int min2 = arr[0];
-            int length = arr.Length;
             for (int i = 0; i < arr.Length; i++)
             {
-                //arr[i] = Convert.ToInt32(Console.ReadLine());
+                arr[i] = Convert.ToInt32(Console.ReadLine());
                 if (arr[i]<=min && arr[i] >= 0)
                 {
                     min = arr[i];
