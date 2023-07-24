d116af2ba66ebd1f54fdcaa9894a5e8ee423618f
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-24 12:01:18 +0530

    [Narenthra Kishore] Add : new Solution Minimum and maximum of an array

--- PraticeProblem/ArrayElements.cs
+++ PraticeProblem/ArrayElements.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace PraticeProblem
 {
-    internal class DuplicateElement
+    internal class ArrayElements
     {
         public static Dictionary<String,int> ArrayToDict()
         {
@@ -60,6 +60,42 @@ namespace PraticeProblem
             {
                 Console.WriteLine("The count of "+item.Key+" is " + item.Value);
             }
+        }
+
+        internal static void GetMinMax()
+        {
+            Console.WriteLine("Enter length of the array");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[length];
+            Console.WriteLine("Enter the array element");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int min, max;
+
+            if (array[0] < array[1])
+            {
+                min = array[0];
+                max = array[1]; 
+            }
+            else
+            {
+                min = array[1];
+                max = array[0];
+            }
+            for (int i = 0; i < length ; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine("the max number is "+max+"\n"+"the minimum number is "+min);
         }
     }
 }
--- PraticeProblem/Program.cs
+++ PraticeProblem/Program.cs
@@ -9,18 +9,22 @@ namespace PraticeProblem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.count of duplicate element");
+                Console.WriteLine("1.count of duplicate element\n2.get unique elements" +
+                    "\n3.get the frequency\n4.Get minimum and maximum of an array");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        DuplicateElement.GetCount();
+                        ArrayElements.GetCount();
                         break;
                     case 2:
-                        DuplicateElement.GetUniqueElement();
+                        ArrayElements.GetUniqueElement();
                         break;
                     case 3:
-                        DuplicateElement.GetFrequency();
+                        ArrayElements.GetFrequency();
+                        break;
+                    case 4:
+                        ArrayElements.GetMinMax();
                         break;
                 }
             }
